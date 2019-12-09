de46f0c1f38ab4d483956dc8066763bf802022a6
cristiana.yasuda <cristiana.yasuda@tivit.com>
2019-12-09 11:20:36 -0300

    change machine

--- Projeto3Burguer/burguerbuild1/src/components/Layout/Layout.js
+++ Projeto3Burguer/burguerbuild1/src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import classes from './Layout.module.css';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+    })
+};
+    render() {
+        return (
+            <Aux>
+        {/*<div>Toolbar, SideDrawer, Backdrop</div>*/}
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    };
+};
+
+export default Layout;
