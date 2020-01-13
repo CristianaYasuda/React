21df63e362723e52060869544f47c930b2ede4ea
cristianayasuda <cristiana.yasuda@tivit.com>
2020-01-13 15:34:54 -0300

    project4 and change projeto3

--- Projeto3Burguer/burguerbuild1/src/App.js
+++ Projeto3Burguer/burguerbuild1/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurguerBuild from './containers/BurguerBuilder/BurguerBuild';
 
 class App extends Component {
